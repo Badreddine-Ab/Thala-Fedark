953767d97769459505b3f0e0a99783f44177a7c7
oumaima <oumaimaabouelhaitam@gmail.com>
2022-12-05 16:12:19 +0100

    create navebar

--- Code/my-app/src/App.js
+++ Code/my-app/src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from './Components/Client/header';
 
 function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      {/* Hello world! */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />} />
+        </Routes>
+      </BrowserRouter>
     </h1>
+       
   );
 }
 
