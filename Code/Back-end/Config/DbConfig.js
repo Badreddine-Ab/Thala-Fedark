3cffbcada2cc15240ebb4d65324ee7333461dc7f
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-12-05 15:36:14 +0100

    connexion à la base de données & definition des variables d'environement

---
+++ Code/Back-end/Config/DbConfig.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+
+const db = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.password, {
+  host: process.env.host,
+  dialect: 'postgres'
+});
+
+
+const connectDb = async ()=>{
+    try {
+        await db.authenticate()
+        console.log("Connection has been established successfully.")
+        
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+module.exports = connectDb
