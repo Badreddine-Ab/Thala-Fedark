aca82524f6af5519172aec0c166680b8b1f1fb37
oumaima <oumaimaabouelhaitam@gmail.com>
2022-12-06 10:27:30 +0100

    add page categorie / modifie api.css

--- Code/my-app/src/App.js
+++ Code/my-app/src/App.js
@@ -2,18 +2,16 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Accueil from './Pages/Client/accueil';
-import Body from './Components/Client/body';
 import PageProcuct from './Components/Client/product';
 
 
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold ">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Accueil />} />
-          <Route path="/body" element={<Body />} />
           <Route path="/PageProcuct" element={<PageProcuct />} />
 
         </Routes>
