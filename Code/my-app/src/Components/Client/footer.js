7a4000ca3ecca4cb1052b31e8655f505e46273a4
oumaima <oumaimaabouelhaitam@gmail.com>
2022-12-07 11:28:49 +0100

    ajouter dossier Accueilpage dans component && remplir fiche app.css && ajouter fiche catgorie et offre et recomended dans rep Accueilpage

--- Code/my-app/src/Components/Client/footer.js
+++ Code/my-app/src/Components/Client/footer.js
@@ -1,50 +1,74 @@
-import React from "react";
-function Footer() {
+export default function Footer() {
     return (
 
-      <footer className="p-4 bg-white rounded-lg shadow md:px-6 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="https://flowbite.com/"
-            className="flex items-center mb-4 sm:mb-0 text-yellow-300"
-          >
-          
-            <span className="self-center text-2xl font-semibold whitespace-nowrap ">
-              thala
-            </span>
-          </a>
-          <ul className="flex flex-wrap items-center mb-6 text-sm  sm:mb-0 ">
-            <li>
-              <a href="#" className="mr-4 hover:  md:mr-6 ">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="mr-4 hover:  md:mr-6">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="#" className="mr-4 hover:  md:mr-6 ">
-                Licensing
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover: ">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-00">
-          © 2022{" "}
-          <a href="https://flowbite.com/" className="hover: ">
-            Flowbite™
-          </a>
-          . All Rights Reserved.
-        </span>
-      </footer>
+      <footer className="bg-white pt-16 pb-12 border-t border-gray-100">
+      <div className="container grid grid-cols-3">
+          <div className="col-span-1 space-y-8">
+          <span className="text-primary">THALA</span>
+              <div className="mr-2">
+                  <p className="text-gray-500">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, hic?
+                  </p>
+              </div>
+              <div className="flex space-x-6">
+                  <a href="#" className="text-gray-400 hover:text-gray-500"><i
+                          className="fa-brands fa-facebook-square"></i></a>
+                  <a href="#" className="text-gray-400 hover:text-gray-500"><i
+                          className="fa-brands fa-instagram-square"></i></a>
+                  <a href="#" className="text-gray-400 hover:text-gray-500"><i
+                          className="fa-brands fa-twitter-square"></i></a>
+                  <a href="#" className="text-gray-400 hover:text-gray-500">
+                      <i className="fa-brands fa-github-square"></i>
+                  </a>
+              </div>
+          </div>
+
+          <div className="col-span-2 grid grid-cols-2 gap-8">
+              <div className="grid grid-cols-2 gap-8">
+                  <div>
+                      <h3 className="text-sm font-semibold text-gray-400 uppercase tracking-wider">Solutions</h3>
+                      <div className="mt-4 space-y-4">
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Marketing</a>
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Analitycs</a>
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Commerce</a>
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Insights</a>
+                      </div>
+                  </div>
+
+                  <div>
+                      <h3 className="text-sm font-semibold text-gray-400 uppercase tracking-wider">Support</h3>
+                      <div className="mt-4 space-y-4">
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Pricing</a>
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Documentation</a>
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Guides</a>
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">API Status</a>
+                      </div>
+                  </div>
+              </div>
+              <div className="grid grid-cols-2 gap-8">
+                  <div>
+                      <h3 className="text-sm font-semibold text-gray-400 uppercase tracking-wider">Solutions</h3>
+                      <div className="mt-4 space-y-4">
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Marketing</a>
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Analitycs</a>
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Commerce</a>
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Insights</a>
+                      </div>
+                  </div>
+
+                  <div>
+                      <h3 className="text-sm font-semibold text-gray-400 uppercase tracking-wider">Support</h3>
+                      <div className="mt-4 space-y-4">
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Pricing</a>
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Documentation</a>
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Guides</a>
+                          <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">API Status</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </footer>
     );
   }
 
-export default Footer;
