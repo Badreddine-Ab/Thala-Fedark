88311e45d79f8f2dc0db15e921d2a1a75e6d4bbc
nafeaanas <nafeaanass15@gmail.com>
2022-12-20 10:21:31 +0100

    update

--- Code/my-app/src/App.js
+++ Code/my-app/src/App.js
@@ -4,8 +4,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Accueil from './Pages/Client/accueil';
 import GetCategory from './Pages/Client/GetProduitByCategory'
 import SearchbyName from './Pages/Client/GetProduitByName'
-
-
+import Login from './Pages/Auth/Login';
+import Register from './Pages/Auth/Register';
+import PageNotfound from './Pages/404.jsx'
 
 
 function App() {
@@ -16,7 +17,9 @@ function App() {
           <Route path="/" element={<Accueil />} />
           <Route path="/:id" element={<GetCategory />} />
           <Route path="search/:id" element={<SearchbyName />} />
-
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register/>}/>
+          <Route path="*" element={<PageNotfound/>}/>
         </Routes>
       </BrowserRouter>
     </h1>
