c43102a6624993b3ba500c8dc27047fe87fde4a9
Badreddine-Ab <badreddine@gmail.com>
2022-12-22 15:57:07 +0100

    adding the search by name function

--- Code/Back-end/Src/Resolver/categorieResolver.js
+++ Code/Back-end/Src/Resolver/categorieResolver.js
@@ -27,7 +27,16 @@ module.exports= {
     
           product: (_, { id }) => Product.findByPk(id, {
             include: [{ model: Categorie }],
-          }), 
+          }),
+          
+          searchProduct: (root, args, context) => {
+            return Product.findOne({
+              where: {
+                name: args.name,
+              },
+            });
+          },
+           
     },
 
     Mutation: {
@@ -87,84 +96,6 @@ module.exports= {
             }
             
           },
-        //   addProduct: (parent, { name, description, stock, images, categorieId }) => {
-        //     return Product.create({ name, description, stock, images, categorieId });
-        //   },
-          updateProduct: (parent, { id, name, description, stock, images, categorieId }) => {
-            return Product.update({ name, description, stock, images, categorieId }, { where: { id } });
-          },
-          deleteProduct: (parent, { id }) => {
-            return Product.destroy({ where: { id } });
-          },
-        //   addProduct: (_, {  name, description, stock, images, categorieId }) => {
-        //     // Use the Sequelize `create` method to create a new product and associate it with the specified category
-        //     return Product.create( {name, description, stock, images, categorieId}, {
-        //       include: [{
-        //         model: Categorie,
-        //         as: 'categorie'
-        //       }]
-        //     });
-        //   }
-        // addProduct: async (_, { input }) => {
-        //   try { 
-        //     // Destructure the images field from the input object
-        //     const { images, ...rest } = input;
-        //     // Extract the categorieId field from the rest object
-        //     const { id: categorieId } = rest;
-            
-        //     // Find the category by its ID
-        //     const categorie = await Categorie.findByPk(categorieId);
-        //     // If the category doesn't exist, throw an error
-        //     console.log(categorieId)
-        //     if (!categorie) {
-        //       throw new ApolloError('Category not found');
-        //     }
-        //     // Create the product with the rest of the input fields and the categorieId
-        //     const product = await Product.create({ ...rest, categorieId });
-        //     // If there are images, save them to the file system
-        //     if (images) {
-        //       await Promise.all(
-        //         images.map(async ({ createReadStream }) => {
-        //           // Generate a unique file name for the image
-        //           const path = `images/${product.id}-${Date.now()}.jpg`;
-        //           // Use the createReadStream function from the image object to read the image data and pipe it to a write stream that saves the image to the file system
-        //           return new Promise((resolve, reject) =>
-        //             createReadStream()
-        //               .pipe(createWriteStream(path))
-        //               .on('finish', () => resolve())
-        //               .on('error', reject)
-        //           );
-        //         })
-        //       );
-        //     }
-        //     // Return the created product
-        //     return product;
-        //   } catch (error) {
-        //     // If an error occurs, throw a GraphQL error
-        //     throw new ApolloError(error);
-        //   }
-        // }
-        // createProduct: async (parent, { input },) => {
-        //   const { images, ...rest } = input;
-        //   const categorie = await Categorie.findByPk(rest.categorieId);
-        //   if (!categorie) {
-        //     throw new Error("Category not found");
-        //   }
-        //   const product = await Product.create(rest);
-        //   console.log(product)
-        //   if (images) {
-        //     const imagePromises = images.map(async (image) => {
-        //       const { createReadStream, filename } = await image;
-        //       const stream = createReadStream();
-        //       const path = `${uuidv4()}-${filename}`;
-        //       await storeUpload({ stream, path });
-        //       return path;
-        //     });
-        //     product.images = await Promise.all(imagePromises);
-        //   }
-        //   await product.save();
-        //   return product;
-        // }
         async createProduct(_, { input }) {
           // Process the uploaded images and save their URLs in the input object
           input.images = input.images.map(file => `http://localhost:9090/${file.path}`);
