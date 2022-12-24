eaea73d9dfd8134566a7bb55a1cd791db20e4c5e
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-12-24 14:49:03 +0100

    fixed duplicate page && fixed default value of product images

--- Code/my-app/src/App.js
+++ Code/my-app/src/App.js
@@ -9,7 +9,6 @@ import Login from './Pages/Auth/Login';
 import Register from './Pages/Auth/Register';
 import PageNotfound from './Pages/404.jsx'
 import Upload from './Components/Client/upload'
-import PageNotfound from './Pages/404.jsx';
 import Produit from './Pages/Admin/produit'
 import Command from './Pages/Admin/command'
 import Cart from './Pages/Client/Cart';
