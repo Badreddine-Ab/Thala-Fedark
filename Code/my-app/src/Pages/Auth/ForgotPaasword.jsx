8143f21dafccce6b4547ebfdb93a5a5a64a074b9
nafeaanas <nafeaanass15@gmail.com>
2022-12-25 23:56:42 +0100

    update Auth

--- Code/my-app/src/Pages/Auth/ForgotPaasword.jsx
+++ Code/my-app/src/Pages/Auth/ForgotPaasword.jsx
@@ -6,26 +6,93 @@ import React from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../../gql/mutations';
+import { FORGOT_PASSWORD } from '../../gql/mutations';
 //import { Lang, useFormInputValidation } from "react-form-input-validation";
 //import axios from "axios";
 //import Cookies from "universal-cookie";
 //const  cookies = new Cookies();
 
-export default function ForgotPasswordForm() {
+export default function FORGOT() {
     const navigate = useNavigate()
     const [formData,setFormData]= useState({})
-    onst [forgotPassword, { loading, error }] = useMutation(FORGOT_PASSWORD_MUTATION);
+    const [foregetPassword,{data,loading,error}] = useMutation(FORGOT_PASSWORD)
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    forgotPassword({ variables: { email: formData.get('email') } });
-  };
+    if (loading) return <h1>Loading...</h1>
+    
 
-  return (
-    <form onSubmit={handleSubmit}>
-      
-    </form>
-        )
+    const handleChange =(e)=> {
+        setFormData({
+            ...formData,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    const handleSubmit = (e)=>{
+        const {email}=formData
+        e.preventDefault()
+        foregetPassword({
+            variables:{
+                email: email
+            }
+        })
+        
+        
+    }
+
+    return (
+        <div className='grid grid-cols-1  h-screen w-full'>
+            
+            <div className='bg-neutral-50 flex flex-col justify-center'>
+                <div className='max-w-[460px] w-full mx-auto bg-white p-5 px-8 rounded-lg shadow-lg'>
+                    {
+                        error &&
+                        <div className='bg-red-50 text-red-400 text-center text-lg rounded-xl transition '>{error.message}</div>
+                    }
+
+                    {
+                        data && data.user &&
+                        <div className='green'>{data.user.name} is SignedUp. You can login now!</div>
+                    }
+                    <div className='space-y-2'>
+                        <a href="">
+                            {/* <img src={Logo} className='w-40' alt="" /> */}
+                        </a>
+                        <p className='text-lg text-gray-600'>Hey welcome back 👋 Login first</p>
+                    </div>
+
+                    
+
+                    
+
+                    <form noValidate autoComplete="off" onSubmit={handleSubmit} className='pt-4 space-y-6'>
+                        <div>
+                            <input type="email"
+                            name="email"
+                            // onBlur={form.handleBlurEvent}
+                            onChange={handleChange}
+                            // value={fields.email}
+                                placeholder='Your email !'
+                                className='w-full px-6 py-3 rounded-xl ring-1 ring-gray-300 invalid:ring-red-500 focus:invalid:ring-red-500 focus:invalid:outline-none disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400'
+                            />
+                            {/* <span className='text-sm text-red-600'>{errors.email ? errors.email : ""}</span>  */}
+                        </div>
+
+                        
+
+                        <div className='flex flex-col'>
+                            <button type='submit' className='block w-full px-7 py-3 rounded-xl bg-yellow-300 hover:bg-yellow-400 focus:bg-yellow-500 active:bg-yellow-500'>
+                                <span className='text-lg text-white'>forgot</span>
+                            </button>
+                            <p className='p-1 text-center'>
+                                <span className='text-yellow-300'><NavLink to="/register">Create new account</NavLink></span>
+                            </p>
+                        </div>
+                    </form>
+
+
+                </div>
+            </div>
+
+        </div>
+    )
 }
--- Code/my-app/src/gql/mutations.jsx
+++ Code/my-app/src/gql/mutations.jsx
@@ -1,26 +1,30 @@
 import {gql} from '@apollo/client'
 export const REGISTER_USER = gql`
-    mutation createUser($userNew:UserInput!){
-        user:registerUser(userNew:$userNew){
-            name
-        }
-    }`
+   mutation Signup($name: String!, $email: String!, $password: String!, $role: String!) {
+  signup(name: $name, email: $email, password: $password, role: $role) {
+    id
+  }
+}`
 
 export const LOGIN_USER =gql`
-    mutation LoginUser($userLogin:UserLoginInput!){
-        user:loginUser(userLogin:userLogin){
-            token
-        }
+mutation Mutation($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    user {
+      id
     }
-`   
-export const FORGOT_PASSWORD_MUTATION = gql`
-  mutation ForgotPasswordMutation($email: String!) {
-    forgotPassword(email: $email)
+    token
   }
+}
+`   
+export const FORGOT_PASSWORD = gql`
+mutation Mutation($email: String!) {
+  foregetPassword(email: $email)
+}
 `;
 
-const RESET_PASSWORD_MUTATION = gql`
-  mutation ResetPasswordMutation($resetToken: String!, $password: String!) {
-    resetPassword(resetToken: $resetToken, password: $password)
-  }
+export const RESET_PASSWORD = gql`
+mutation Mutation($password: String!, $token: String!) {
+  resetPassword(password: $password, token: $token)
+}
+
 `;
